b4690c88322c755f9f1dc49f71c62a97bacb6f7d
syui <syui@syui.cf>
2020-05-09 22:07:19 +0900

    first

---
+++ readme.md
@@ -0,0 +1,9 @@
+
+
+```sh
+$ cargo build
+$ ./target/debug/udrs
+
+$ udrs --help
+$ udrs "foo%20bar"
+```
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use url::percent_encoding::percent_decode;
+
+use structopt::StructOpt;
+use failure::Error;
+pub type Result<T> = std::result::Result<T, Error>;
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(name = "INPUT")]
+    input: String,
+}
+
+fn main() -> Result<()> {
+    let opt = Opt::from_args();
+    Ok(println!("{}", decode(&opt.input)?))
+}
+
+//fn main() -> Result<()> {
+//    let args: Vec<String> = std::env::args().collect();
+//    let input = &args[1];
+//
+//    Ok(println!("{}", decode(input)?))
+//}
+
+fn decode(input: &str) -> Result<String> {
+    let decoded = percent_decode(input.as_bytes()).decode_utf8()?;
+    Ok(decoded.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::decode;
+
+    #[test]
+    fn decode_space_ok() {
+        let expected = "foo bar";
+        let input = "foo%20bar";
+        let actual = decode(input).unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn decode_ascii_ok() {
+        let expected = r##" !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ `"##;
+        let input = r##"%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2D%2E%2F0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ `"##;
+        let actual = decode(input).unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    #[should_panic]
+    fn decode_invalid_utf8_ng() {
+        let input = "%93%FA%96%7B%8C%EA%0D%0A";
+        decode(input).unwrap();
+    }
+}
