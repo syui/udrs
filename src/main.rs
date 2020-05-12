dbcf5db7a5a363b78feb2cf872f2f4d137de1a61
syui <syui@syui.cf>
2020-05-12 16:25:02 +0900

    fix test

--- src/main.rs
+++ src/main.rs
@@ -44,46 +44,20 @@ fn ud(_c: &Context) {
     println!("{}", percent_decode(&_c.args[0].as_bytes()).decode_utf8().unwrap());
 }
 
-fn decode(input: &str) -> String {
-    percent_decode(input.as_bytes()).decode_utf8().unwrap().to_string()
-}
-
-fn encode(input: &str) -> String {
-    base64::encode(input)
-}
-
 #[cfg(test)]
 mod tests {
-
-    use crate::decode;
-    use crate::encode;
 
     #[test]
     fn decode_space_ok() {
         let expected = "foo bar";
         let input = "foo%20bar";
-        let actual = decode(input);
+        let actual = url::percent_encoding::percent_decode(input.as_bytes()).decode_utf8().unwrap();
         assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn decode_ascii_ok() {
-        let expected = r##" !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ `"##;
-        let input = r##"%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2D%2E%2F0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ `"##;
-        let actual = decode(input);
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    #[should_panic]
-    fn decode_invalid_utf8_ng() {
-        let input = "%93%FA%96%7B%8C%EA%0D%0A";
-        decode(input);
     }
 
     #[test]
     fn base64_encode() {
         let input = "hello";
-        encode(input);
+        base64::encode(input);
     }
 }
